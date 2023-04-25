19e2f857e26356ee592f1358ba04a1d652426f6f
leejunghwan <hwan@sungil-i.kr>
2023-04-26 00:39:42 +0900

    master

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var db = FirebaseFirestore.instance;
+
+  void fb() {
+    // Create a new user with a first and last name
+    final user = <String, dynamic>{
+      "first": "Ada",
+      "last": "Lovelace",
+      "born": 1815
+    };
+
+// Add a new document with a generated ID
+    db.collection("users").add(user).then((DocumentReference doc) =>
+        // ignore: avoid_print
+        print('DocumentSnapshot added with ID: ${doc.id}'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            onPressed: fb, child: const Icon(Icons.play_arrow_outlined)),
+      ),
+    );
+  }
+}
